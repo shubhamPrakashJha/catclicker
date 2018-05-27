6288926caca8ca4645d6bdbc8eb329fe6d201049
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-05-27 19:01:12 +0530

    count click on each of the two cat

--- js/script.js
+++ js/script.js
@@ -3,9 +3,20 @@
  */
 $(function () {
     var i = 0;
-    $('img#cat').click(function () {
+    var j = 0;
+    cat1 = $('#cat1');
+    cat2 = $('#cat2');
+    cat1.find('.name').text('cat 1');
+    cat2.find('.name').text('cat 2');
+
+    $('#cat1 img#cat').click(function () {
         // console.log('hello');
         i++;
-        $('#count').text(i)
+        $('#cat1 #count').text(i)
+    });
+    $('#cat2 img#cat').click(function () {
+        // console.log('hello');
+            j++;
+        $('#cat2 #count').text(j)
     });
 });
